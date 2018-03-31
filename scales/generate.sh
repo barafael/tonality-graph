403aa8b2399eaf0f08d205bed67a170ee6f2bcc2
Rafael Bachmann <mediumendian@gmail.com>
2018-03-31 15:38:16 +0200

    prevent simultaneous playback

--- scales/generate.sh
+++ scales/generate.sh
@@ -4,5 +4,12 @@ rm -rfv audio
 mkdir audio
 
 for sheet in $(ls *.mscz); do
-    mscore $sheet -o "audio/$sheet.mp3";
+    #convert files to .mp3 using musescore CLI
+    msconverted="audio/$sheet.mp3"
+    mscore $sheet -o $msconverted;
+
+    # remove silence at end using sox
+    name="${msconverted/.mscz/}"
+    sox $msconverted $name reverse silence 1 0.1 0.5% reverse
+    rm $msconverted
 done
